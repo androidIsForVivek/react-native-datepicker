392088848129c63321a0cb636b8b84f488200e96
Nishant Gupta <nishant.gupta1@goibibo.com>
2021-05-06 19:04:45 +0530

    Update datepicker.js

--- datepicker.js
+++ datepicker.js
@@ -185,7 +185,7 @@ class DatePicker extends Component {
     );
   }
 
-  onDateChange(date) {
+  onDateChange(event, date) {
     this.setState({
       allowPointerEvents: false,
       date: date
@@ -398,7 +398,7 @@ class DatePicker extends Component {
                         mode={mode}
                         minimumDate={minDate && this.getDate(minDate)}
                         maximumDate={maxDate && this.getDate(maxDate)}
-                        onDateChange={this.onDateChange}
+                        onChange={this.onDateChange}
                         minuteInterval={minuteInterval}
                         timeZoneOffsetInMinutes={timeZoneOffsetInMinutes ? timeZoneOffsetInMinutes : null}
                         style={[Style.datePicker, customStyles.datePicker]}
